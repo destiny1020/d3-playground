77812bb0556f7a2eceab6d383c369f545fa1ff29
destiny1020 <destiny.jiang@gmail.com>
2015-06-02 12:16:33 +0800

    color settings

--- ch3/soccer.js
+++ ch3/soccer.js
@@ -19,15 +19,21 @@ function createSoccerViz() {
 
 		var teamG = d3.selectAll('g.overallG');
 		teamG.append('circle')
+			.attr('r', 0)
+			.transition()
+			.delay(function(d, i){ return i * 100; })
+			.duration(500)
+			.attr('r', 40)
+			.transition()
+			.duration(500)
 			.attr('r', 20)
-			.style('fill', 'pink')
 			.style('stroke', 'black')
 			.style('stroke-width', '1px');
 
 		teamG.append('text')
 			.style('text-anchor', 'middle')
 			.attr('y', 30)
-			.style('font-size', '10px')
+			// .style('font-size', '10px')
 			.text(function(d) {
 				return d.team;
 			});
@@ -43,7 +49,7 @@ function createSoccerViz() {
 			.enter()
 			.append('button')
 			.attr('class', 'teams')
-			.on('click', buttonClick)
+			.on('click', buttonClick3)
 			.html(function(d) {
 				return d;
 			});
@@ -54,6 +60,9 @@ function createSoccerViz() {
 			});
 
 			var radiusScale = d3.scale.linear().domain([0, maxValue]).range([2, 20]);
+			// var ybRamp = d3.scale.linear().domain([0, maxValue]).range(['yellow', 'blue']);
+			// OTHER: interpolateHcl, interpolateLab
+			var ybRamp = d3.scale.linear().interpolate(d3.interpolateHsl).domain([0, maxValue]).range(['yellow', 'blue']);
 
 			d3.selectAll('g.overallG')
 				.select('circle')
@@ -62,10 +71,61 @@ function createSoccerViz() {
 				.attr('r', function(d) {
 					return radiusScale(d[datapoint]);
 				})
+				.style('fill', function(d) {
+					return ybRamp(d[datapoint]);
+				});
 		}
 
+		// use category10
+		function buttonClick2(datapoint) {
+			var maxValue = d3.max(incomingData, function(el) {
+				return parseFloat(el[datapoint]);
+			});
+
+			var tenColorScale = d3.scale.category10(
+					['UEFA', 'CONMEBOL', 'CAF', 'AFC']
+				);
+			var radiusScale = d3.scale.linear().domain([0, maxValue]).range([2, 20]);
+
+			d3.selectAll('g.overallG')
+				.select('circle')
+				.transition()
+				.duration(1000)
+				.style('fill', function(p) { return tenColorScale(p.region); })
+				.attr('r', function(p) { return radiusScale(p[datapoint]); });
+		}
+
+		// use colorbrewer --- quantize
+		function buttonClick3(datapoint) {
+			var maxValue = d3.max(incomingData, function(el) {
+				return parseFloat(el[datapoint]);
+			});
+
+			var colorQuantize = d3.scale.quantize()
+									.domain([0, maxValue])
+									.range(colorbrewer.Reds[3]);
+			var radiusScale = d3.scale.linear().domain([0, maxValue])
+									.range([2, 20]);
+
+			d3.selectAll('g.overallG')
+				.select('circle')
+				.transition()
+				.duration(1000)
+				.style('fill', function(p) {
+					return colorQuantize(p[datapoint]);
+				})
+				.attr('r', function(p) {
+					return radiusScale(p[datapoint]);
+				});
+
+		}
+
+		// disable the pointer events on text
+		teamG.select('text')
+			.style('pointer-events', 'none');
+
 		// mouseover/out processing
-		teamG.on('mouseover', highlightRegion);
+		teamG.on('mouseover', highlightRegion2);
 		function highlightRegion(d) {
 			d3.selectAll('g.overallG')
 				.select('circle')
@@ -74,10 +134,31 @@ function createSoccerViz() {
 				});
 		}
 
+		function highlightRegion2(d, i) {
+			d3.select(this)
+				.select('text')
+				.classed('active', true)
+				.attr('y', 10);
+			d3.selectAll('g.overallG')
+				.select('circle')
+				.each(function(p, i) {
+					p.region == d.region ?
+						d3.select(this).classed('active', true) :
+						d3.select(this).classed('inactive', true);
+				});
+
+			// re-appending the current node
+			this.parentElement.appendChild(this);
+		}
+
 		teamG.on('mouseout', function() {
 			d3.selectAll('g.overallG')
 				.select('circle')
-				.style('fill', 'pink');
+				.attr('class', '');
+			d3.selectAll('g.overallG')
+				.select('text')
+				.classed('active', false)
+				.attr('y', 30);
 		});
 	}
 }
